dba131f532d69d4db7805c52d97e92559ae53b2a
rezayudakusumah <sebutsajamentari@gmail.com>
2020-01-08 20:45:37 +0700

    Adding balance calculation feature

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -1,5 +1,42 @@
 ActiveAdmin.register Payment do
 
-  permit_params :amount, :user_id
+  # permit_params :amount, :user_id
   
+  controller do
+
+    def create
+      @payment = Payment.new(payment_params)
+  
+      if @payment.save
+        @user_id = @payment.user_id
+        @user_balance = Payment.where(user_id: @user_id).sum(:amount)
+        User.where(id: @user_id).update(balance: @user_balance)
+        flash[:info] = "Success created new data"
+        redirect_to admin_payment_path(@payment)
+      else
+        flash[:danger] = "Something went wrong"
+        render "new"
+      end
+    end
+
+    def destroy
+      @payment = Payment.find(params[:id])
+      @payment.destroy
+
+      @user_id = @payment.user_id
+      @user_balance = Payment.where(user_id: @user_id).sum(:amount)
+      User.where(id: @user_id).update(balance: @user_balance)
+      
+      flash[:info] = "Success deleted data"
+      redirect_to admin_payments_path
+    end
+
+    private
+
+    def payment_params
+      params.require(:payment).permit(:amount, :user_id)
+    end
+
+  end
+
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,21 @@
 ActiveAdmin.register User do
 
   permit_params :name, :identity_card, :address, :contact, :balance
-  
+
+  form do |f|
+    inputs "Details" do
+      input :name
+      input :identity_card
+      input :address
+      input :contact
+    end
+    actions
+  end
+
+  # controller do
+
+  #   def update
+    
+  #   end
+  # end
 end
